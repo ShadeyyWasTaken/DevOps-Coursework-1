f97da107948e0ba4f3144c0758286c02580d9b57
ShadeyyWasTaken <94585123+ShadeyyWasTaken@users.noreply.github.com>
2022-11-15 20:27:46 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 in1="50 pounds"
 ex1=$'50.0 Dollars = 37 Pounds\n50.0 Dollars = 44 Euros'
-out1="$(java -cp CurrencyConverter.java $in1)"
+out1="$(java -cp CurrencyConverter $in1)"
 
 echo "$in1"
 echo "$ex1"
